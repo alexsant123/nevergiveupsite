9ef9f5f1e52c7216f7e6a5d73ac4b59a411374b1
alexsant123 <eua2016@hotmail.com>
2026-01-13 23:53:02 -0300

    new features

---
+++ src/app/pages/english-survivor/english-survivor.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-english-survivor',
+  standalone: true,
+  templateUrl: './english-survivor.component.html',
+  styleUrls: ['./english-survivor.component.css']
+})
+export class EnglishSurvivorComponent {}
